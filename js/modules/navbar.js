a9c931ac94ef1fc458f9a05e0329159908c72805
Ailer-h <henriqueailer2@gmail.com>
2026-03-27 00:16:01 -0300

    Added tab navigation and a few of the tabs that are gonna be used.

--- js/modules/navbar.js
+++ js/modules/navbar.js
@@ -1,5 +1,6 @@
 import { logout } from "./logout";
 import { change_theme } from "./themeController";
+import { check_session } from "./supabase";
 
 const menu_functionalities = {
     profile: function(){alert("wip")},
@@ -7,7 +8,41 @@ const menu_functionalities = {
     logout: logout,
 }
 
-export function start_navbar_controller() {
+const navigation_tabs = {
+
+    payments: {
+        address: "paymentMethods.html",
+        label: "Payment methods"
+    },
+    income: {
+        address: "income.html",
+        label: "Income sources"
+    },
+    expenses: {
+        address: "expenses.html",
+        label: "Expenses"
+    }
+    
+}
+
+function add_navigation_menu() {
+
+    let navigation_menu = ""
+
+    Object.entries(navigation_tabs).forEach(([id, props]) => {
+
+        if (!window.location.href.includes(props.address)){
+            navigation_menu += `<p class="navigation" id="${id}">${props.label}</p>`
+        }
+    })
+
+    navigation_menu += $(".menu").html()
+
+    $(".menu").html(navigation_menu)
+
+}
+
+function start_navbar_controller() {
 
     let menu = $(".menu-items")
 
@@ -30,5 +65,19 @@ export function start_navbar_controller() {
     Object.entries(menu_functionalities).forEach(([id, func]) => {
         document.getElementById(id).addEventListener("click", func)
     })
+
+    $(".navigation").each(function() {
+        $(this).click(function() {
+            check_session("login.html", false)
+
+            window.location.href = navigation_tabs[$(this).attr('id')].address
+
+        })
+    })
     
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    add_navigation_menu();
+    start_navbar_controller();
+})
