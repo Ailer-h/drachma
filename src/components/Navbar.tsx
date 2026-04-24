729628d18a4acf4a4f64d922e963623b456842da
Ailer-h <henriqueailer2@gmail.com>
2026-04-24 12:48:36 -0300

    Added navigation to the navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import "../stylesheets/Navbar.css"
 import UserMenu from "./UserMenu";
 import { useState } from "react";
+import PageNavigator from "./PageNavigator";
 
 interface NavbarProps {
     type: string
@@ -39,6 +40,7 @@ const Navbar = ({type}: NavbarProps) => {
                 {
                     type == "main" &&
                     <div className="menu">
+                        <PageNavigator/>
 
                         <button onClick={toggleMenu}>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M166.15-274.15v-36.93h627.7v36.93h-627.7Zm0-187.7v-36.92h627.7v36.92h-627.7Zm0-187.69v-36.92h627.7v36.92h-627.7Z"/></svg>
---
+++ src/components/PageNavigator.tsx
@@ -0,0 +1,25 @@
+import { useLocation, useNavigate } from "react-router-dom"
+
+const PageNavigator = () => {
+
+    const Navigate = useNavigate();
+    const Location = useLocation();
+
+    const routes = [
+        { address: "/paymentMethods", label: "Payment methods" },
+        { address: "/income", label: "Income sources" },
+        { address: "/expenses", label: "Expenses" }
+    ].filter(route => route.address !== Location.pathname)
+
+    console.log(routes)
+
+    return <>{
+        routes.map((route) => {
+            return <p className="navigation" onClick={() => {Navigate(route.address)}}>{route.label}</p>
+        })
+    }</>
+    
+
+}
+
+export default PageNavigator
